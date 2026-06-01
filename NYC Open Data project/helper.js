8fc697ba643c7ebe6ec7bd3baa24f524ff552926
Khloe Islam <khloei2@nycstudents.net>
2026-06-01 17:17:10 +0000

    fhutr

--- NYC Open Data project/helper.js
+++ NYC Open Data project/helper.js
@@ -21,7 +21,7 @@ function showMap(lat,lon){
 //card
 function card( info ){
   let build = `<div class="card fitted center">
-    <h2>Market Name: ${info.marketname}</h2>
+    <h2>${info.marketname}</h2>
               <hr>
               <p>Location: ${info.streetaddress}, ${info.borough}</p>
               <p>Year Opened: ${info.year}</p>
@@ -29,8 +29,9 @@ function card( info ){
               <p>Operating hours:${info.daysoperation}, ${info.hoursoperations}</p>
               <p>Open year round?:${info.open_year_round}</p>
               <p>Accepts EBT?: ${info.accepts_ebt}</p>`;
-              if(info.latitude && info.longitude){
-               build += `<input type=“button” value=“Map” onclick=“showMap( ${info.latitude}, ${info.longitude})">`;
+             
+              if(info.latitude  && info.longitude){
+                  build += `<input type="button" value="Map" onclick="showMap(${info.latitude},${info.longitude})">`;
                   }
      build +=    `</div>`;
 return build;
